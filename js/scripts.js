273193c6f7e94d72682f5abbab4821767a9a2250
z-mojomoji <suphicha.anu@gmail.com>
2017-06-22 22:50:37 +0700

    All functionals and other sections have been made, left with blog section

--- js/scripts.js
+++ js/scripts.js
@@ -23,13 +23,46 @@ $('.NavigationBar-link').click(function() {
     }
 });
 
-// navigation scroll lijepo radi materem
-// $('nav a').click(function(event) {
-//     var id = $(this).attr("href");
-//     var offset = 70;
-//     var target = $(id).offset().top - offset;
-//     $('html, body').animate({
-//         scrollTop: target
-//     }, 500);
-//     event.preventDefault();
-// });
+$(document).ready(function(){
+
+var slnum= 0; //current Slide
+var maxnum = ($(".Slider-page").length)-1; //number of all slides
+var pageWidth = $(".Slider-container").innerWidth(); //get the width of container
+
+$('.Slider-page').css('width', pageWidth);
+
+var toSlide = $(".Slider-page").innerWidth(); //width per slide
+
+$(window).resize(function() {
+    toSlide = $(".Slider-page").innerWidth(); //width per slide
+});
+    
+$('.Slider').on('click','.Slider-paginationDot',function(){
+    var getPage=$(this).attr('pagenum');
+    slnum = parseInt(getPage);
+    $('.Slider-paginationDot').removeClass('Slider-paginationDot--active');
+    $('.page'+slnum).addClass('Slider-paginationDot--active');
+    var lefarrnum = -(parseInt(slnum)*toSlide);//change width of a page
+    $('.Slider-containerList').animate({left:lefarrnum},{queue: false, duration: 300});
+});
+    
+// clear auto slide
+$('.Slider').on('mouseover',function(){
+	window.clearInterval(timerA);
+}).on('mouseout',function(){
+	timerA = window.setInterval("autoslide()", 5000);
+});    
+
+});
+
+// auto slide1
+var btnnum = 0;
+var timerA;
+timerA = window.setInterval("autoslide()", 5000);
+function autoslide(){ 
+    var lastSlide = ($(".Slider-page").length)-1; //number of all slides 
+	btnnum = parseInt(btnnum)+1;
+	if(btnnum>lastSlide){btnnum=0;}//change page number accordingly
+	jQuery('.Slider-paginationDot.page'+ btnnum).click();
+}
+ 
